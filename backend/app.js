d3278bc34394021d24aad9626605deaed2367f7e
Chinnu-2005 <gkrvkoushik@gmail.com>
2025-12-26 17:53:21 +0530

    v1

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 
 const authRoutes = require('./API/routes/authRoutes');
 const doubtRoutes = require('./API/routes/doubtRoutes');
